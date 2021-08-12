099180b57205acd3caa27eaabb9cd51a545e2775
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-08-12 13:12:21 -0500

    finally got the routes to work and seeded the db for the first time, now to test

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const helpers = require('./utils/helpers');
+
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
@@ -38,5 +39,5 @@ app.use(require('./controllers/'));
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-	app.listen(PORT, () => console.log('Now listening'));
+	app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 });
