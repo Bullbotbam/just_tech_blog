034c642b62258a8c9b7748f0c65388a2b058c402
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-14 23:57:07 -0500

    connect to jawsdb

--- server.js
+++ server.js
@@ -14,6 +14,23 @@ const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PW,
+		{
+			host: 'localhost',
+			dialect: 'mysql',
+			port: 3306,
+		}
+	);
+}
+
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
