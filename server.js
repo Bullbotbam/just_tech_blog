2acf43eb5ca0a32e65fbea32bbaa12cd3eeb8e0b
Donald Bull <81042354+Bullbotbam@users.noreply.github.com>
2021-09-15 00:42:42 -0500

    correcting connection.js to heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-	secret: process.env.PSST_SECRET,
+	secret: 'sususudio',
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
